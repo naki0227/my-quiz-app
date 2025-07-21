59305d55d17bd60e2048bc64ee9800864aac2bdd
naki0227 <ibuki.n5@icloud.com>
2025-07-21 13:08:48 +0900

    Add quiz

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const allQuizData = {
     "第一回": [
         {
-            question: "1）の空所を補充してください",
+            question: "1)の空所を補充してください",
             pdfPath: "sportsPhysiology.pdf",
             pageNumber: 12,
             answer: "エネルギー"
         },
         {
-            question: "2）の空所を補充してください",
+            question: "2)の空所を補充してください",
             pdfPath: "sportsPhysiology.pdf",
             pageNumber: 13,
             answer: "アデノシン三リン酸(ATP)"
         },
         {
-            question: "3）の空所を補充してください",
+            question: "3)の空所を補充してください",
             pdfPath: "sportsPhysiology.pdf",
             pageNumber: 14,
             answer: "ATP→ADP時にエネルギー発生"
         },
         {
-            question: "4）の空所を補充してください",
+            question: "4)の空所を補充してください",
             pdfPath: "sportsPhysiology.pdf",
             pageNumber: 15,
             answer: "高エネルギーリン酸結合が<br>外れるときにエネルギーが発生"
         },
         {
-            question: "7）,8)の空所を補充してください",
+            question: "7),8)の空所を補充してください",
             pdfPath: "sportsPhysiology.pdf",
             pageNumber: 17,
             answer: "7)食物など外からエネルギー<br>8)無機リン酸(Pi)"
@@ -37,17 +37,255 @@ const allQuizData = {
             answer: "9)3つのエネルギー供給系<br>10)同時に使われている<br>11)代謝環境に応じて<br>12)相互作用しながら"
         },
         {
-            question: "13）,14),15)の空所を補充してください",
+            question: "13),14),15)の空所を補充してください",
             pdfPath: "sportsPhysiology.pdf",
             pageNumber: 18,
             answer: "13)ATP再生機構(ATP-CP系)<br>14)無酸素的解糖系(乳酸系)<br>15)有酸素系(酸化系)"
         },
     ],
     "第2回": [
-
+        {
+            question: "1),2)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 26,
+            answer: "1)約5mmol/L<br>2)数回の筋収縮"
+        },
+        {
+            question: "3),4)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 26,
+            answer: "3)クレアチンリン酸(PCr)<br>4)ATPの約5倍量"
+        },
+        {
+            question: "5),6)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 26,
+            answer: "5)クレアチンリン酸(PCr)+ADP→クレアチン(Cr)+ATP<br>6)高速エネルギー供給システム"
+        },
+        {
+            question: "7),8),9)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 28,
+            answer: "7)グルコース(糖質)<br>8)小腸に吸収<br>9)血液"
+        },
+        {
+            question: "10),11),12)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 28,
+            answer: "10)筋肉に吸収<br>11)ピルビン酸に分解<br>12)ATPの再合成"
+        },
+        {
+            question: "13),14),15),16)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 29,
+            answer: "13)ピルビン酸<br>14)ミトコンドリア<br>15)有酸素系<br>16)乳酸"
+        },
+        {
+            question: "17),18)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 29,
+            answer: "17)筋繊維内<br>18)肝臓"
+        },
+        {
+            question: "19),20),21),22),23)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 31,
+            answer: "19)ピルビン酸<br>20)アセチルCoA(アセチルコリンエー)<br>21)ミトコンドリア内<br>23)クエン酸回路<br>23)二酸化炭素と水"
+        },
+        {
+            question: "24),25),26)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 32,
+            answer: "24)ミトコンドリア<br>25)約40兆個<br>26)100~3000個"
+        },
+        {
+            question: "27),28),29)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 33,
+            answer: "27)脚などの骨格筋細胞<br>28)神経細胞<br>29)心筋細胞"
+        },
+        {
+            question: "30),31)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 34,
+            answer: "30)持久力が向上する<br>31)エネルギー(ATP)"
+        },
+        {
+            question: "32),33),34),35)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 35,
+            answer: "32)ATPが不足<br>33)ADpまでも分解<br>34)AMPキナーゼ<br>35)体積が増える"
+        },
+        {
+            question: "36),37),38)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 36,
+            answer: "36)無酸素<br>37)無酸素<br>38)有酸素"
+        },
+        {
+            question: "39),40),41)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 36,
+            answer: "39)ミトコンドリア<br>40)非常に多い<br>41)遅い"
+        },
     ],
     "第3回": [
-
+        {
+            question: "1),2),3),4)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 42,
+            answer: "1)ガス交換<br>2)ATPの再合成<br>3)酸素<br>4)二酸化炭素"
+        },
+        {
+            question: "5)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 43,
+            answer: "5)口及び鼻から酸素を取り込み、<br>二酸化炭素を排出する期間"
+        },
+        {
+            question: "6),7),8)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 43,
+            answer: "6)口腔<br>7)鼻腔<br>8)咽頭"
+        },
+        {
+            question: "9),10),11)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 43,
+            answer: "9)喉頭<br>10)気管<br>11)気管支"
+        },
+        {
+            question: "12),13),14)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 43,
+            answer: "12)右肺<br>13)左肺<br>14)細気管支"
+        },
+        {
+            question: "15),16),41)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 44,
+            answer: "15)細気管支<br>16)肺胞<br>17)胸膜"
+        },
+        {
+            question: "18),19),20)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 44,
+            answer: "18)細気管支から枝分かれ<br>19)肺胞<br>20)ぶどうの房"
+        },
+        {
+            question: "21)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 45,
+            answer: "大きさ：直径約0.1mm<br>広げると：テニスコート1面分<br>個数：約3億個"
+        },
+        {
+            question: "22）,23)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 45,
+            answer: "22)酸素を細胞へ渡し<br>23)二酸化炭素や老廃物を受け取る"
+        },
+        {
+            question: "24),25),26),27)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 46,
+            answer: "24)横隔膜<br>25)肋間筋<br>26)内肋間筋<br>27)外肋間筋"
+        },
+        {
+            question: "28),29),30),31)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 46,
+            answer: "28)胸郭が収縮<br>29)横隔膜が緩む<br>30)胸郭が拡張<br>31)横隔膜が収縮"
+        },
+        {
+            question: "32),33)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 47,
+            answer: "32)腹式呼吸<br>33)胸式呼吸"
+        },
+        {
+            question: "34）,35),36)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 47,
+            answer: "34)12回〜18回<br>35)不随意に調整<br>36)随意に変える"
+        },
+        {
+            question: "37),38)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 47,
+            answer: "37)自律神経<br>38)唯一自分の意思でも<br>コントロール可能"
+        },
+        {
+            question: "39),40),41)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 48,
+            answer: "39)運動皮質<br>40)呼吸中枢(脳の延髄)<br>41)呼吸筋へ"
+        },
+        {
+            question: "42),43),44)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 49,
+            answer: "42)肺換気<br>43)毎分換気量<br>44)1回換気量"
+        },
+        {
+            question: "45)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 49,
+            answer: "45)1回換気量 x 1分間の呼吸数 = 分時換気量"
+        },
+        {
+            question: "46),47),48)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 50,
+            answer: "46)肺胞の膜と毛細血管の壁<br>47)肺胞<br>48)毛細血管"
+        },
+        {
+            question: "49),50)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 50,
+            answer: "49)拡散<br>50)濃度が高い方から低い方"
+        },
+        {
+            question: "51),52),53),54)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 51,
+            answer: "51)外呼吸(ガス交換)<br>52)内呼吸(細胞呼吸)<br>53)酸素化ヘモグロビン<br>54)脱酸素化ヘモグロビン"
+        },
+        {
+            question: "55),56)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 51,
+            answer: "55)酸素飽和度<br>56)約95~98%"
+        },
+        {
+            question: "57),58),59)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 52,
+            answer: "57)約3000~4000ml<br>58)約2000~3000ml<br>59)0.5L"
+        },
+        {
+            question: "60),61),62)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 52,
+            answer: "60)予備吸気量<br>61)予備呼気量<br>62)残機量"
+        },
+        {
+            question: "63),64),65)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 53,
+            answer: "63)全ての酸素を使用しているわけではない<br>64)解剖学的死腔<br>65)約30%程度を占める(約150ml)"
+        },
+        {
+            question: "66),67)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 53,
+            answer: "66)生理学的死腔<br>67)生理学的死腔に肺胞死腔を加えたスペース"
+        },
+        {
+            question: "68)の空所を補充してください",
+            pdfPath: "sportsPhysiology.pdf",
+            pageNumber: 54,
+            answer: "68)使用しなかった酸素量は動静使用格差から算出可能"
+        },
     ],
     "第4回": [
 
@@ -78,7 +316,10 @@ const allQuizData = {
     ],
     "第13回": [
 
-    ]
+    ],
+    "第14回": [
+
+    ],
 };
 
 let questions = [];
